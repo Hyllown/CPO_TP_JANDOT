7b49cbd63fa485d11115a0d38d91a62a4858633d
Achille Jandot <achille@cessfil.net>
2024-09-17 12:59:45 +0200

    creation d'un nouveau projet versionné

---
+++ Exo1/src/exo1/Exo1.java
@@ -0,0 +1,27 @@
+/*
+*TP0 Exercice 1
+*AJ
+*Septembre 2024
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author achil
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         String prenom;
+         Scanner sc;
+         sc = new Scanner(System.in);
+         System.out.println("Bonjour, quel est votre prenom ?");
+         prenom = sc.nextLine();
+    }
+    
+}
