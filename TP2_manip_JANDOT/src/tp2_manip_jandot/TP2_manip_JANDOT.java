fdd96fba0a3b7bcc4087f361507287e063a65fc6
Achille Jandot <achille@cessfil.net>
2024-10-15 20:28:59 +0200

    creation d'un nouveau projet versionné

---
+++ TP2_manip_JANDOT/src/tp2_manip_jandot/TP2_manip_JANDOT.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_manip_jandot;
+
+/**
+ *
+ * @author achil
+ */
+public class TP2_manip_JANDOT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Tartiflette assiette1 = new Tartiflette(500) ; 
+        Tartiflette assiette2 = new Tartiflette(600) ; 
+        
+        Moussaka [] tabMouss = new Moussaka[10];
+        tabMouss[0] = new Moussaka(0);
+        tabMouss[1] = new Moussaka(10);
+        tabMouss[2] = new Moussaka(20);
+        tabMouss[3] = new Moussaka(30);
+        tabMouss[4] = new Moussaka(40);
+        tabMouss[5] = new Moussaka(50);
+        tabMouss[6] = new Moussaka(60);
+        tabMouss[7] = new Moussaka(70);
+        tabMouss[9] = new Moussaka(80);
+        tabMouss[8] = new Moussaka(90);
+    }
+    
+}
